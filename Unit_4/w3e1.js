f9e4f53c29fb09276cfc69c585fac8eff1af627a
Esteban <estebanfj95@gmail.com>
2018-10-19 13:17:41 +0200

    Working...

--- Unit_4/w3e1.js
+++ Unit_4/w3e1.js
@@ -1,22 +1,27 @@
-// Crea un formulario típico de registro para tu web. Debe contener al menos campos para introducir
-// nombre, apellidos, DNI, Teléfono, email y nombre de usuario. Todos los campos son obligatorios,
-// y debes validar el mayor número posible de ellos haciendo uso de expresiones regulares.
-// El nombre de usuario debe estar formado por al menos 8 caracteres, entre los cuales debe aparecer
-// obligatoriamente algún número y algún signo de puntuación.
-//
-// Conforme sale el foco de cada input, deberás validar el contenido de éste y mostrar información
-// al usuario que le indique si está correcto o no.
+function isValid(input, reg_exp) {
+    input.style.border = (reg_exp.test(input.value.trim())) ? "2px solid green" : "2px solid red";
+}
 
-var lInfoNombre = document.getElementById("infoNombre");
-var lInfoApellidos = document.getElementById("infoApellidos");
-var lInfoDni = document.getElementById("infoDni");
-var lInfoTelefono = document.getElementById("infoTelefono");
-var lInfoEmail = document.getElementById("infoEmail");
-var lInfoNombreUsuario = document.getElementById("infoNombreUsuario");
+document.getElementById("tfNombre").addEventListener("keyup", function(e) {
+    isValid(e.target, /^[A-Z][a-z]+([\s-][A-Z][a-z]+)*$/);
+});
+
+document.getElementById("tfApellidos").addEventListener("keyup", function(e) {
+    isValid(e.target, /^[A-Z][a-z]+([\s-][A-Z][a-z]+)+$/);
+});
+
+document.getElementById("tfDni").addEventListener("keyup", function(e) {
+    isValid(e.target, /^\d{8}[A-Z]$/);
+});
+
+document.getElementById("tfTelefono").addEventListener("keyup", function(e) {
+    isValid(e.target, /^\d{9}$/);
+});
 
 document.getElementById("tfEmail").addEventListener("keyup", function(e) {
-    if (/\S+@{1}\S+\.{1}\S/.test(e.target.value))
-        e.target.style.border = "2px solid green";
-    else
-        e.target.style.border = "2px solid red";
+    isValid(e.target, /^[a-zA-Z0-9_.]+@[a-z0-9]+\.[a-z]+$/);
+});
+
+document.getElementById("tfNombreUsuario").addEventListener("keyup", function(e) {
+    isValid(e.target, /^(?=.*\d)(?=.*[.,:;¡!¿?])[a-zA-Z0-9.,:;¡!¿?]{8,}$/);
 });
