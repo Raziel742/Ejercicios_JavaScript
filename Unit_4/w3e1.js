9dd23b6c082ed8dd6a660f45af0d9330be17cbe2
Esteban <estebanfj95@gmail.com>
2018-10-20 13:36:54 +0200

    Working...

--- Unit_4/w3e1.js
+++ Unit_4/w3e1.js
@@ -1,27 +1,12 @@
-function isValid(input, reg_exp) {
-    input.style.border = (reg_exp.test(input.value.trim())) ? "2px solid green" : "2px solid red";
+function isValid(inputId, reg_exp) {
+    document.getElementById(inputId).addEventListener("keyup", function(e) {
+        e.target.style.border = (reg_exp.test(e.target.value.trim())) ? "2px solid green" : "2px solid red";
+    });
 }
 
-document.getElementById("tfNombre").addEventListener("keyup", function(e) {
-    isValid(e.target, /^[A-Z][a-z]+([\s-][A-Z][a-z]+)*$/);
-});
-
-document.getElementById("tfApellidos").addEventListener("keyup", function(e) {
-    isValid(e.target, /^[A-Z][a-z]+([\s-][A-Z][a-z]+)+$/);
-});
-
-document.getElementById("tfDni").addEventListener("keyup", function(e) {
-    isValid(e.target, /^\d{8}[A-Z]$/);
-});
-
-document.getElementById("tfTelefono").addEventListener("keyup", function(e) {
-    isValid(e.target, /^\d{9}$/);
-});
-
-document.getElementById("tfEmail").addEventListener("keyup", function(e) {
-    isValid(e.target, /^[a-zA-Z0-9_.]+@[a-z0-9]+\.[a-z]+$/);
-});
-
-document.getElementById("tfNombreUsuario").addEventListener("keyup", function(e) {
-    isValid(e.target, /^(?=.*\d)(?=.*[.,:;¡!¿?])[a-zA-Z0-9.,:;¡!¿?]{8,}$/);
-});
+isValid("tfNombre", /^[A-Z][a-z]+([\s-][A-Z][a-z]+)*$/);
+isValid("tfApellidos", /^[A-Z][a-z]+([\s-][A-Z][a-z]+)+$/);
+isValid("tfDni", /^\d{8}[A-Z]$/);
+isValid("tfTelefono", /^\d{9}$/);
+isValid("tfEmail", /^[a-zA-Z0-9_.]+@[a-z0-9]+\.[a-z]+$/);
+isValid("tfNombreUsuario", /^(?=.*\d)(?=.*[.,:;¡!¿?])[a-zA-Z0-9.,:;¡!¿?]{8,}$/);
